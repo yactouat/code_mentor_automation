f4ff3903b75b68c09e8741359d05bc527f5404a7
yactouat <yactouat@hotmail.com>
2022-10-29 16:42:50 +0200

    adding explanatory modal on  conf on session lead signup

--- src/Processes/Process.php
+++ src/Processes/Process.php
@@ -3,9 +3,13 @@
 namespace Udacity\Processes;
 
 abstract class Process {
+
     protected $errors = [];
+
+    // TODO test that no process using Gmail can be run if no Gmail email configured
 
     public function getErrors(): array {
         return $this->errors;
     }
+
 }
