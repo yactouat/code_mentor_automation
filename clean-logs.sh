f0de14669cd258ce464510e58e2c147eeb3fb125
yactouat <yactouat@hotmail.com>
2022-10-29 23:14:26 +0200

    adding removal of db logs in pre commit hook

--- clean-logs.sh
+++ clean-logs.sh
@@ -3,4 +3,5 @@ sudo rm data/logs/nginx/access.log
 sudo rm data/logs/nginx/error.log
 sudo rm data/logs/php/cli.log
 sudo rm data/logs/php/web_app.log
+sudo rm data/logs/php/db.log
 sudo rm tests/fixtures/logs/php/db.log
