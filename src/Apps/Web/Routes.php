e4a1b20b522f30e0b1816151a1a8e2526ff0d2f5
yactouat <yactouat@hotmail.com>
2022-10-30 15:14:36 +0100

    adding routes for emails to behind students

--- src/Apps/Web/Routes.php
+++ src/Apps/Web/Routes.php
@@ -14,13 +14,15 @@ final class Routes {
      */
     public static function getRegisteredRoutes(): array {
         return [
-            "GET" => [
+            'GET' => [
                 '/' => ['Resource\SessionLeadsController', 'index'],
+                '/emails/behind-students' => ['EmailsController', 'index'],
                 'login' => ['Resource\SessionLeadsController', 'login'],
                 'logout' => ['Resource\SessionLeadsController', 'logout'],
                 'session-leads/create' => ['Resource\SessionLeadsController', 'create']
             ],
             'POST' => [
+                '/emails/behind-students' => ['EmailsController', 'sendEmailsToBehindStudents'],
                 'login' => ['Resource\SessionLeadsController', 'login'],
                 'session-leads/create' => ['Resource\SessionLeadsController', 'persist']
             ]
--- tests/Unit/Apps/Web/RoutesTest.php
+++ tests/Unit/Apps/Web/RoutesTest.php
@@ -9,13 +9,15 @@ final class RoutesTest extends TestCase {
 
     public function testGetRegisteredRoutesReturnsExpectedRoutes() {
         $expected = [
-            "GET" => [
+            'GET' => [
                 '/' => ['Resource\SessionLeadsController', 'index'],
+                '/emails/behind-students' => ['EmailsController', 'index'],
                 'login' => ['Resource\SessionLeadsController', 'login'],
                 'logout' => ['Resource\SessionLeadsController', 'logout'],
                 'session-leads/create' => ['Resource\SessionLeadsController', 'create']
             ],
             'POST' => [
+                '/emails/behind-students' => ['EmailsController', 'sendEmailsToBehindStudents'],
                 'login' => ['Resource\SessionLeadsController', 'login'],
                 'session-leads/create' => ['Resource\SessionLeadsController', 'persist']
             ]
