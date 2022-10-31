9c4a58c02eb5cdc328307916f2b86142e11fd643
yactouat <yactouat@hotmail.com>
2022-10-31 20:00:34 +0100

    better logs cleaner

--- scripts/sh/clean-logs.sh
+++ scripts/sh/clean-logs.sh
@@ -13,3 +13,9 @@ sudo rm data/logs/php/db.log
 sudo rm tests/fixtures/logs/php/cli.log
 sudo rm tests/fixtures/logs/php/web_app.log
 sudo rm tests/fixtures/logs/php/db.log
+
+# removing uploaded csvs
+sudo rm sudo rm data/csv/*.csv
+
+# removing sessions
+sudo rm sudo rm data/sessions/sess_*
