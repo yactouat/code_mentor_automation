355d5f93005d3885f25d8cc7af448735129b2961
yactouat <yactouat@hotmail.com>
2022-10-31 20:53:27 +0100

    simpler authentication trait in integration tests

--- tests/Integration/Apps/Web/AuthenticateTrait.php
+++ tests/Integration/Apps/Web/AuthenticateTrait.php
@@ -3,24 +3,15 @@
 namespace Tests\Integration\Apps\Web;
 
 use Udacity\Apps\Web\Controllers\Resource\SessionLeadsController;
-use Udacity\Database;
+use Udacity\Models\SessionLeadModel;
 
 trait AuthenticateTrait {
 
-    protected Database $database;
-
     protected function authenticate(string $email = 'test@gmail.com', string $firstName = 'Yacine'): void {
+        $sessionLead = new SessionLeadModel($email, $firstName, 'test g app password', 'test user password');
+        $sessionLead->persist();
         $ctlr = new SessionLeadsController();
         $_SERVER['REQUEST_METHOD'] = 'POST';
-        $_POST = [
-            'submit' => '1',
-            'email' => $email,
-            'first_name' => $firstName,
-            'google_app_password' => 'test google app password',
-            'user_passphrase' => 'test user password',
-        ];
-        $ctlr->persist();
-        $_SESSION = []; // resetting the session since `persist` fills it
         $_POST = [
             'submit' => '1',
             'email' => $email,
